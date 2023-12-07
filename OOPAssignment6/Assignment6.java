011cde07dee69110cd90a7263736f0f52a381bad
shreyashingle2004@gmail.com <shreyashingle2004@gmail.com>
2023-12-07 15:56:05 +0530

    intial commit

--- OOPAssignment6/Assignment6.java
+++ OOPAssignment6/Assignment6.java
@@ -1,46 +1,46 @@
-package OOPAssignment6;
-
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
-public class Assignment6 {
+public class Exception_6 {
     public static void main(String[] args) {
-
-        try {
-            int Num1, Num2;
+        try{
+            int Num1;
+            int Num2;
 
             InputStreamReader in = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(in);
-            System.out.println("Enter Number 1:");
+            System.out.println("Enter the Number1: ");
             Num1 = Integer.parseInt(br.readLine());
-            System.out.println("Enter Number 2:");
+
+            System.out.println("Enter the Number2: ");
             Num2 = Integer.parseInt(br.readLine());
-            int result = Num1 / Num2;
-            if (Num2 == 0) {
+
+            if (Num2 ==0){
                 throw new ArithmeticException();
             }
-            System.out.println("The Division of Numbers is: " + result);
+
+            int result = Num1/Num2;
+
+            System.out.println("The Division of Number is: "+result);
 
             int[] a = new int[4];
-            a[0] = 1;
-            a[1] = 2;
-            a[2] = 3;
-            a[3] = 4;
-            System.out.println("Enter the Index to access the element");
-            int k = Integer.parseInt(br.readLine());
-            System.out.println(a[k]);
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(br.readLine());
+            }
+            System.out.println("Enter Index of element to access:");
+            int access = Integer.parseInt(br.readLine());
+            System.out.println(a[access]);
 
-        } catch (ArithmeticException e) {
-            System.out.println("Cannot Divide by zero!!" + e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Please Enter Integers only!");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Array index out of Bounds!!");
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        }catch (ArithmeticException e){
+            System.out.println("Cannot divide by zero");
+        } catch (NumberFormatException e ){
+            System.out.println("Please Enter integers only");
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index out of bounds");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
